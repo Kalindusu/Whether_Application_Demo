3b6ffe2190ea3add33be5015e2ed631516f3e50c
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-02-18 11:30:23 +0530

    weather py

--- weather.py
+++ weather.py
@@ -9,3 +9,14 @@ load_dotenv()
 def get_current_weather(city=" Walasmulla City"):
 
      request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+     weather_data=requests.get(request_url).json()
+     return weather_data
+
+if__name__ == "__main__": # type: ignore
+print('\n*** Get Current Weather Condition ***\n')
+city = input('\nEnter city name: ')
+weather_data = get_current_weather(city)
+
+print('\n')
+pprint(weather_data)
